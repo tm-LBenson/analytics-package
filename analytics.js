7730b7c799818763157b3c361daaa015b71dcf7b
tm-LBenson <tm.lewisbenson@gmail.com>
2023-05-02 17:39:53 -0400

    implement location instead of IP tracking

--- analytics.js
+++ analytics.js
@@ -42,19 +42,24 @@ const displayConsentBanner = (onAccept) => {
   if (document.getElementById('consent-accept')) {
     return;
   }
+  const consent = localStorage.getItem('analytics-consent');
+
+  if (consent === 'declined') {
+    return;
+  }
 
   const banner = document.createElement('div');
   banner.innerHTML = `
-    <div style="position: fixed; bottom: 0; background: #000; color: #fff; width: 100%; padding: 15px; text-align: center; z-index: 9999;">
-      We use cookies to collect analytics data.
-      <button id="consent-accept" style="background: #4CAF50; border: none; color: white; padding: 5px 10px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; margin: 4px 2px; cursor: pointer;">Accept</button>
-      <button id="consent-decline" style="background: #f44336; border: none; color: white; padding: 5px 10px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; margin: 4px 2px; cursor: pointer;">Decline</button>
-    </div>
+  <div style="position: fixed; bottom: 0; background: #000; color: #fff; width: 100%; padding: 15px; text-align: center; z-index: 9999;">
+  Our website uses cookies to analyze traffic and personalize your experience.<br>
+  <span style="font-size: 12px;">We only collect basic information such as device type and screen size to help us optimize your experience on our site. We do not collect personally identifiable information.</span>
+  <button id="consent-accept" style="background: #4CAF50; border: none; color: white; padding: 5px 10px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; margin: 4px 2px; cursor: pointer;">Accept</button>
+  <button id="consent-decline" style="background: #f44336; border: none; color: white; padding: 5px 10px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; margin: 4px 2px; cursor: pointer;">Decline</button>
+</div>
   `;
 
   document.body.appendChild(banner);
 
-  console.log('Displaying consent banner');
   banner.querySelector('#consent-accept').addEventListener('click', () => {
     localStorage.setItem('analytics-consent', 'accepted');
     banner.remove();
@@ -74,6 +79,22 @@ const checkConsent = (justAccepted = false) => {
   const consent = localStorage.getItem('analytics-consent');
   return consent === 'accepted';
 };
+const getLocationData = async (ipAddress) => {
+  try {
+    const response = await fetch(`https://ipapi.co/${ipAddress}/json/`);
+    const data = await response.json();
+
+    // You can return the whole data object or only the parts you need
+    return {
+      country: data.country_name,
+      region: data.region,
+      city: data.city,
+    };
+  } catch (error) {
+    console.error('Error getting location data: ', error);
+    return null;
+  }
+};
 
 async function analytics(siteName, clientId) {
   try {
@@ -87,6 +108,7 @@ async function analytics(siteName, clientId) {
     }
 
     const ipAddress = await getIpAddress();
+    const location = await getLocationData(ipAddress);
 
     const data = {
       siteName,
@@ -96,7 +118,7 @@ async function analytics(siteName, clientId) {
         height: window.innerHeight,
       },
       deviceType: getDeviceType(),
-      ipAddress,
+      location,
     };
 
     const response = await fetch(
